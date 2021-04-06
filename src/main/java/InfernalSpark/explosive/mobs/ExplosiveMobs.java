abf202b4a65b6b23d79996bb40ecbface2ca7a18
InfernalSpark <71075261+InfernalSpark@users.noreply.github.com>
2021-04-06 14:34:01 +0530

    Initial Commit

---
+++ src/main/java/InfernalSpark/explosive/mobs/ExplosiveMobs.java
@@ -0,0 +1,14 @@
+package InfernalSpark.explosive.mobs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExplosiveMobs implements ModInitializer {
+	public static final String modid = "explosivemobs";
+
+
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/InfernalSpark/explosive/mobs/mixin/LivingEntityMixin.java
@@ -0,0 +1,26 @@
+package InfernalSpark.explosive.mobs.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(at = @At("HEAD"), method = "tick")
+    public void tick(CallbackInfo info){
+        if(this.isOnFire()){
+            world.createExplosion(null, this.getX(), this.getY(), this.getZ(), 2.0F, Explosion.DestructionType.DESTROY);
+        }
+    }
+}
