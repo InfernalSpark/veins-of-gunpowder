ba27290f2b54bd6e81eb8dea6ca6773d12c00a27
InfernalSpark <71075261+InfernalSpark@users.noreply.github.com>
2021-04-07 14:32:02 +0530

    Removed Player from explosion list

--- src/main/java/InfernalSpark/explosive/mobs/mixin/MobEntityMixin.java
+++ src/main/java/InfernalSpark/explosive/mobs/mixin/MobEntityMixin.java
@@ -1,9 +1,8 @@
 package InfernalSpark.explosive.mobs.mixin;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,10 +10,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity {
-    public LivingEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
     }
 
     @Inject(at = @At("HEAD"), method = "tick")
